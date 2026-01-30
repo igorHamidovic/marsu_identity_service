f46c723361545bd4d81231f9ce411fccfc95a783
Igor Hamidovic <igorweg@outlook.com>
2026-01-30 16:34:39 +0100

    Fixed opsional attributes

--- src/identity/dtos.py
+++ src/identity/dtos.py
@@ -11,17 +11,17 @@ class ChallengeResponse(BasicApiModel):
 
 
 class IdentifyAppRequest(BasicApiModel):
-    device_name: Optional[str]
-    device_type: Optional[str]
-    brand: Optional[str]
-    manufacturer: Optional[str]
-    model_name: Optional[str]
-    os_name: Optional[str]
-    os_version: Optional[str]
-    platform_api_level: Optional[str]
-    app_version: Optional[str]
-    build_number: Optional[str]
-    platform: Optional[str]
+    device_name: Optional[str] = None
+    device_type: Optional[str] = None
+    brand: Optional[str] = None
+    manufacturer: Optional[str] = None
+    model_name: Optional[str] = None
+    os_name: Optional[str] = None
+    os_version: Optional[str] = None
+    platform_api_level: Optional[str] = None
+    app_version: Optional[str] = None
+    build_number: Optional[str] = None
+    platform: Optional[str] = None
 
 class IdentifyAppResponse(BasicApiModel):
     installation_id: str
